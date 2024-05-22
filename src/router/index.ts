dcdbfcefb6f4ca258c8aa372e5dc790f453d56b5
Bruno Freire <brunoqm99@gmail.com>
2024-05-22 08:42:05 -0400

    refactor: Remove Explorer route from router

--- src/router/index.ts
+++ src/router/index.ts
@@ -3,8 +3,6 @@ import { RouteRecordRaw } from 'vue-router';
 import TabsPage from '../views/TabsPage.vue'
 import Session from '@/views/Session.vue';
 import Sessions from '@/views/Sessions.vue';
-import Login from '@/views/Login.vue';
-import Explorer from '@/views/Explorer.vue';
 
 const routes: Array<RouteRecordRaw> = [
   {
@@ -18,14 +16,6 @@ const routes: Array<RouteRecordRaw> = [
   {
     path: '/sessions',
     component: Sessions
-  },
-  {
-    path: '/explorer',
-    component: Explorer
-  },
-  {
-    path: '/login',
-    component: Login
   }
 ]
 
